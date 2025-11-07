5bd7cbdc528264edeefa0998fc52751c8ea90e1b
Kautz <felipemkautzmann@gmail.com>
2025-11-07 17:02:57 +0100

    feat: first endpoint

--- src/atlas/config/fast_api.py
+++ src/atlas/config/fast_api.py
@@ -18,11 +18,11 @@ class AtlasApi(BaseSettings):
     api_title: str = Field(
         alias="title", default="Default API Name", validation_alias="ATLAS_API_NAME"
     )
-    description: str = Field(
+    description: str = Field(alias="description"
         default="API for Atlas application. This will play around with the Spotify API and databases.",
         validation_alias="DESCRIPTION",
     )
-    docs_url: str = Field(default="/", validation_alias="DOCS_URL")
+    docs_url: str = Field(alias="docs_url",default="/", validation_alias="DOCS_URL")
     openapi_url: str = Field(default="/openapi.json", validation_alias="OPENAPI_URL")
     redirect_slashes: bool = True
 
